3673f13569cbe706bf52b42f1d1890a6b8e5dcd7
ariasjp76-pixel <ariasjp76@gmail.com>
2025-12-18 12:28:22 -0600

    Update script.js
    Signed-off-by: ariasjp76-pixel <ariasjp76@gmail.com>

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 ======================================================
 // BASE DE DATOS LOCAL
 // ======================================================
@@ -294,4 +295,5 @@ function rechazarNotificacion(i) {
     cargarNotificaciones();
 
 }
+
 
